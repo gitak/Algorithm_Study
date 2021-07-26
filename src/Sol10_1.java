2f50bef8caf47c19403a80d645eb5c9651208c99
gitak <gitak0127@gmail.com>
2021-07-26 17:54:58 +0900

    solved problem

--- src/Sol10_1.java
+++ src/Sol10_1.java
@@ -8,6 +8,7 @@ public class Sol10_1 {
         int [] guild = new int[n];
         int max = 0;
         int count = 0; //그룹의 수
+        int remainder = 0; //그룹으로 안묶인 나머지 인원 수
         for(int i = 0 ; i < n; i++){
             guild[i] = sc.nextInt();
         }
@@ -26,7 +27,16 @@ public class Sol10_1 {
         }
 
         for(int i = 0; i < max; i++){ // 공포도가 적은 인원수부터 그룹으로 묶을 수 있는 갯수
-            count += check[i] / (i+1);
+            count += check[i] / (i+1) ;
+            check[i] = check[i] % (i+1);
+            if((check[i] + remainder) / (i+1) != 0 ){ //나머지 인원과 현재인원을 합쳤을 때 하나의 그룹이 되는 경우
+                count++;
+                check[i] = check[i] - remainder;
+                remainder = check[i];
+            }
+            else{
+                remainder += check[i];
+            }
         }
         System.out.println(count);
 
