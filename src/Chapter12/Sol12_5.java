da006b77efa9c48333ad410e38fc9e16917f02de
gitak <gitak0127@gmail.com>
2021-09-07 19:00:15 +0900

    Solved Problem

--- src/Chapter12/Sol12_5.java
+++ src/Chapter12/Sol12_5.java
@@ -30,42 +30,23 @@ public class Sol12_5 {
            direction[i] = temp[1];
         }
 
-        int[][] snake = new int[k][2]; //뱀의 몸통위치를 담을 2차원 배열
+        int[][] snake = new int[k+1][2]; //뱀의 몸통위치를 담을 2차원 배열
         int head_x = 0, head_y= 0;
-        int prev_x = 0, prev_y = 0;
-        int count = 0; //뱀이 사과를 먹은 횟수
+        int count = 0; //뱀의 사과를 먹은 횟수
         int rotate = 0;
         int[] dx = {0,1,0,-1}; //시계방향으로 회전했을 때 인덱스값 변화
         int[] dy = {1,0,-1,0};
         int time = 0, t= 0;
         boolean check = false;
 
+        int x1, x2, y1, y2;
+
         while(true){
             time++;
             head_x += dx[rotate]; head_y += dy[rotate]; // head의 위치
-
-            if(second[t] == time){
-                switch(direction[t]){ //방향전환
-                    case "D":
-                        rotate++;
-
-                        if(t <l-1) t++;
-                        if(rotate == 4) rotate = 0;
-                        break;
-                    case "L":
-                        rotate--;
-                        if(t <l-1) t++;
-                        if(rotate == -1) rotate = 3;
-                        break;
-                }
-            }
-
-            if(head_x>= n || head_x < 0 || head_y >= n|| head_y < 0){ //뱀이 맵 밖으로 나가는 경우
-                break;
-            }
-            else if(count >= 4){
-                //뱀이 몸통이랑 부딫히는 경우
-                for(int i = 0; i < count; i++){
+            System.out.println(head_x+" "+head_y);
+            if(count > 3){ //뱀이 몸통이랑 부딫히는 경우
+                for(int i = 1; i <= count; i++){
                     if(head_x == snake[i][0] && head_y == snake[i][1]){
                         check = true;
                         break;
@@ -73,19 +54,83 @@ public class Sol12_5 {
                 }
                 if(check) break;
             }
+            if(second[t] == time){
+                if(t < l-1) {
+                    switch (direction[t]) { //방향전환
+                        case "D":
+                            rotate++;
+                            if (rotate == 4) rotate = 0;
+                            break;
+                        case "L":
+                            rotate--;
+                            if (rotate == -1) rotate = 3;
+                            break;
+                    }
+                    t++;
+                }
+                else{ //t = l-1인 경우
+                    switch (direction[t]) { //방향전환
+                        case "D":
+                            rotate++;
+                            if (rotate == 4) rotate = 0;
+                            break;
+                        case "L":
+                            rotate--;
+                            if (rotate == -1) rotate = 3;
+                            break;
+                    }
+                }
+
+                 //뱀이 방향전환 후 이동하는 경우
+
+                    if(map[head_x][head_y] == 1){ //이동한 칸에 사과가 있는 경우
+                        count++;
+                        map[head_x][head_y] = 0; // 먹은 사과 없애기
+                        x1 = snake[0][0];
+                        y1 = snake[0][1];
+                        snake[0][0] = head_x;
+                        snake[0][1] = head_y;// 몸통첫번째의 위치
+                        for (int i = 1; i <= count; i++) { //뱀의 몸통위치 바꾸기
+                            x2 = snake[i][0];
+                            y2 = snake[i][1];
+                            snake[i][0] = x1; //몸통의 위치 갱신
+                            snake[i][1] = y1;
+                            x1 = x2;
+                            y1 = y2;
+                        }
+                    }
+
+                    //이동한 칸에 사과가 없는 경우
+                    else {
+                        x1 = snake[0][0];
+                        y1 = snake[0][1];
+                        snake[0][0] = head_x;
+                        snake[0][1] = head_y;// 몸통첫번째의 위치
+                        for (int i = 1; i <= count; i++) { //뱀의 몸통위치 바꾸기
+                            x2 = snake[i][0];
+                            y2 = snake[i][1];
+                            snake[i][0] = x1; //몸통의 위치 갱신
+                            snake[i][1] = y1;
+                            x1 = x2;
+                            y1 = y2;
+                        }
+                    }
+            }
+
+             else if(head_x >= n || head_x < 0 || head_y >= n|| head_y < 0){ //뱀이 맵 밖으로 나가는 경우
+                break;
+            }
+
             else{ //뱀이 이동하는 경우
 
                 if(map[head_x][head_y] == 1){ //이동한 칸에 사과가 있는 경우
                     count++;
-                }
-                //뱀위 몸의 길이가 2이상인 경우
-                int x1, x2, y1, y2;
-                if(count >= 1) {
+                    map[head_x][head_y] = 0; // 먹은 사과 없애기
                     x1 = snake[0][0];
                     y1 = snake[0][1];
-                    snake[0][0] = prev_x;
-                    snake[0][1] = prev_y;
-                    for (int i = 1; i < count; i++) { //뱀의 몸통위치 바꾸기
+                    snake[0][0] = head_x;
+                    snake[0][1] = head_y;// 몸통첫번째의 위치
+                    for (int i = 1; i <= count; i++) { //뱀의 몸통위치 바꾸기
                         x2 = snake[i][0];
                         y2 = snake[i][1];
                         snake[i][0] = x1; //몸통의 위치 갱신
@@ -94,9 +139,30 @@ public class Sol12_5 {
                         y1 = y2;
                     }
                 }
-                prev_x = head_x; prev_y = head_y;
 
+                //이동한 칸에 사과가 없는 경우
+                else {
+                    x1 = snake[0][0];
+                    y1 = snake[0][1];
+                    snake[0][0] = head_x;
+                    snake[0][1] = head_y;// 몸통첫번째의 위치
+                    for (int i = 1; i <= count; i++) { //뱀의 몸통위치 바꾸기
+                        x2 = snake[i][0];
+                        y2 = snake[i][1];
+                        snake[i][0] = x1; //몸통의 위치 갱신
+                        snake[i][1] = y1;
+                        x1 = x2;
+                        y1 = y2;
+                    }
+                }
             }
+
+            for(int i = 0; i <= count; i++){
+                for(int j = 0; j < 2;j++){
+                    System.out.print(snake[i][j]+"  ");
+                }
+            }
+            System.out.println();
         }
 
         System.out.println(time);
