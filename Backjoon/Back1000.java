3ba3bcfc9a0a9da0a5944841aea67d93b7b2e1d9
gitak <gitak0127@gmail.com>
2021-10-06 15:15:50 +0900

    init

--- Backjoon/Back1000.java
+++
@@ -1,7 +0,0 @@
-package Backjoon;
-
-public class Back1000 {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ Backjoon/Back1987.java
@@ -0,0 +1,24 @@
+package Backjoon;
+import java.util.Scanner;
+
+public class Back1987 {
+    static char[][] map;
+    public static void main(String[] args) {
+        //입력시작
+        Scanner sc = new Scanner(System.in);
+        int r = sc.nextInt(); //세로의 길이
+        int c = sc.nextInt(); //가로의 길이
+        map = new char[r][c];
+        sc.nextLine(); //버퍼 지우기
+
+        for(int i = 0; i < r; i++){
+            String temp = sc.nextLine();
+            for(int j = 0; j < c; j++){
+                map[i][j] = temp.charAt(j);
+            }
+        }
+        sc.close(); //입력 종료
+        //Logic
+
+    }
+}
