4817e3dde55e26f80f24dccfa45c2b3f7f9e3892
gitak <78472689+gitak@users.noreply.github.com>
2021-10-06 14:15:17 +0900

    Delete src/MP03 directory

--- src/MP03/Observable.java
+++
@@ -1,7 +0,0 @@
-package MP03;
-
-public interface Observable {
-    void addObserver(Observer o);
-    void removeObserver(Observer o);
-    void notifyObservers();
-}
--- src/MP03/Observer.java
+++
@@ -1,5 +0,0 @@
-package MP03;
-
-public interface Observer {
-    void updateText(String s);
-}
