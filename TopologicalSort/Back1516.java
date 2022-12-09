7b01cd94b32518cb21a64b752bd5406ce0f9705c
gitak <gitak0127@gmail.com>
2022-12-09 18:06:56 +0900

    TopologicalSort

---
+++ TopologicalSort/Back1516.java
@@ -0,0 +1,74 @@
+package TopologicalSort;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class Back1516 {
+
+    static int N;
+    static int[] indeg, time, time_done;
+    static ArrayList<Integer>[] adj;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        input();
+        logic();
+    }
+
+    private static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        N = Integer.parseInt(br.readLine());
+        indeg = new int[N + 1];
+        time = new int[N + 1];
+        time_done = new int[N + 1];
+        adj = new ArrayList[N + 1];
+        for (int i = 1; i <= N; i++) {
+            adj[i] = new ArrayList<>();
+        }
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            time[i] = Integer.parseInt(st.nextToken());
+            while (st.hasMoreTokens()) {
+                int x = Integer.parseInt(st.nextToken());
+                if(x == -1) break;
+                adj[x].add(i);
+                indeg[i]++;
+            }
+        }
+    }
+
+    static void logic() {
+        Deque<Integer> queue = new LinkedList<>();
+        //제일 앞에 "정렬될 수 있는" 정점 찾기
+        for (int i = 1; i <= N; i++) {
+            if(indeg[i] == 0) queue.add(i);
+        }
+
+        //위상정렬 순서대로 time_done 계산을 함께 해주기
+        while (!queue.isEmpty()) {
+            int x = queue.poll();
+            for (int y : adj[x]) {
+                time_done[y] = Math.max(time_done[y],time[x]);
+                indeg[y]--;
+
+                if (indeg[y] == 0) {
+                    time[y] += time_done[y];
+                    queue.add(y);
+                }
+            }
+        }
+
+        for (int i = 1; i <= N; i++) {
+            sb.append(time[i]).append('\n');
+        }
+
+        System.out.println(sb);
+
+    }
+}
